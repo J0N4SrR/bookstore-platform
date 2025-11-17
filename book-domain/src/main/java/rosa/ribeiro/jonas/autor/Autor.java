ce7d04e1d5b952b1fd69239c019335b85096c43a
Jonas Ribeiro <krnsjrr@gmail.com.br>
2025-11-17 10:47:26 -0300

    feat(book-domain): implementação completa das entidades do domínio Livro(Autor, Editora, Categoria)

--- book-domain/src/main/java/rosa/ribeiro/jonas/autor/Autor.java
+++ book-domain/src/main/java/rosa/ribeiro/jonas/autor/Autor.java
@@ -1,4 +1,55 @@
 package rosa.ribeiro.jonas.autor;
 
+import jakarta.persistence.*;
+import rosa.ribeiro.jonas.livro.Livro;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name ="autores")
 public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String nome;
+    private LocalDate dataNascimento;
+    private String nacionalidade;
+
+    @ManyToMany(mappedBy = "autores")
+    private List<Livro> livros = new ArrayList<>();
+
+    protected Autor() {}
+
+    public Autor(String nome, LocalDate dataNascimento, String nacionalidade) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.nacionalidade = nacionalidade;
+    }
+
+    public void adicionarLivro(Livro livro) {
+        this.livros.add(livro);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
--- book-domain/src/main/java/rosa/ribeiro/jonas/editora/Editora.java
+++ book-domain/src/main/java/rosa/ribeiro/jonas/editora/Editora.java
@@ -1,4 +1,59 @@
 package rosa.ribeiro.jonas.editora;
 
+import jakarta.persistence.*;
+import rosa.ribeiro.jonas.livro.Livro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "editoras")
 public class Editora {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String cnpj;
+
+    private String telefone;
+    private String email;
+
+    @OneToMany(mappedBy = "editora")
+    private List<Livro> livrosPublicados = new ArrayList<>();
+
+    protected Editora() {}
+
+    public Editora(String nome, String cnpj, String telefone, String email) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Livro> getLivrosPublicados() {
+        return livrosPublicados;
+    }
 }
